579c85e9b4c09a928d0044403e7708e09530ef3b
xdffish <xdffish@qq.com>
2024-06-27 17:11:50 +0800

    翻译为中文

--- active_rollback.sql
+++ active_rollback.sql
@@ -1,16 +1,16 @@
-REM Script Name		: active_rollback.sql
-REM Author			: Craig Richards
-REM Created			:
-REM Last Modified	:
-REM Version			: 1.0
+REM 脚本名称		: active_rollback.sql
+REM 作者			: Craig Richards
+REM 创建时间		:
+REM 最后修改		:
+REM 版本			: 1.0
 REM
-REM Modifications	:
+REM 修改记录		:
 REM
-REM Description		: Shows active rollback segments
+REM 描述		: 显示活动回滚段
 
 SELECT SUBSTR(r.name,1,5) RSN,s.sid SID, SUBSTR(nvl(s.username, 'No Tran'),1,7) USR, s.osuser OSUSR, s.terminal TERM, o.rssize/(1024*1024) RSIZE
 FROM   v$lock l, v$session s, v$rollname r, v$rollstat o
-WHER   l.sid = s.sid(+)
+WHERE  l.sid = s.sid(+)
 AND    TRUNC(l.id1/65536) = r.usn
 AND    o.usn = r.usn
 AND    l.type = 'TX'
